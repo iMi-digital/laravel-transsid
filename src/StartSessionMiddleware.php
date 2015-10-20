3c4fa66e642df5a8c2f2304c3bfdac131cefe4ec
Alexander Menk <a.menk@imi.de>
2015-10-20 18:27:11 +0200

    Lock Session to IP + Agent

--- src/StartSessionMiddleware.php
+++ src/StartSessionMiddleware.php
@@ -5,10 +5,54 @@ namespace iMi\LaravelTransSid;
 class StartSessionMiddleware extends \Illuminate\Session\Middleware\StartSession
 {
 
+    const LOCKED_FIELD = 'locked_to';
+
+    /**
+     * Store IP and Agent in order to lock the session to a specfic user
+     * (against over-taking via URL sharing)
+     *
+     * @param $session
+     * @param $request
+     */
+    protected function lockToUser($session, $request)
+    {
+        $session->set(self::LOCKED_FIELD, [
+            'ip' => $request->getClientIp(),
+            'agent' => md5($request->server('HTTP_USER_AGENT'))
+        ]);
+    }
+
+    /**
+     * Check if IP or Agent changed
+     *
+     * @param $session
+     * @param $request
+     * @return bool
+     */
+    protected function validate($session, $request)
+    {
+        $locked = $session->get(self::LOCKED_FIELD);
+        return !($locked['ip'] != $request->getClientIp()
+            || $locked['agent'] != md5($request->server('HTTP_USER_AGENT')));
+    }
+
     public function getSession(\Illuminate\Http\Request $request)
     {
         $session = $this->manager->driver();
         $session->setId($request->input($session->getName()));
+
+        $session->start();
+        if (!$session->has(self::LOCKED_FIELD)) {
+            $this->lockToUser($session, $request);
+        } else {
+            // validate session against store IP and user agent hash
+            if (!$this->validate($session, $request)) {
+                $session->setId(null); // refresh ID
+                $session->start();
+                $this->lockToUser($session, $request);
+            }
+        }
+
         return $session;
     }
 }
